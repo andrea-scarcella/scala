b2fa3feb2bcdaab7a205b155d06f81e0d9c7d86e
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2015-11-21 00:39:43 +0100

    Refactored sqrt to function block with nested functions

--- functional_programming_scala/week1/newton_sqrt/src/main/scala/NewtonSqrt.scala
+++ functional_programming_scala/week1/newton_sqrt/src/main/scala/NewtonSqrt.scala
@@ -3,16 +3,18 @@ import scala.math._
 
 class NewtonSqrt{
 
+
+def sqrt(x:Double)={
+
 def sqrtIter(guess:Double, x:Double):Double=
 if (isGoodEnough(guess,x))guess
 else sqrtIter(improve(guess,x),x)
 
-def isGoodEnough(guess:Double, x:Double)=
-abs(guess*guess/x - 1)<0.001 
+def isGoodEnough(guess:Double, x:Double)= abs(guess*guess/x - 1)<0.001 
 
-def improve(guess:Double, x:Double)=
-(guess+x/guess)/2
+def improve(guess:Double, x:Double)= (guess+x/guess)/2
 
-def sqrt(x:Double)=sqrtIter(x/2,x)
+sqrtIter(x/2,x)
+}
 
 }
