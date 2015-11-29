30578a9d9b184966f69c5c0f65e1489ce30890b6
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2015-11-29 23:22:15 +0100

    Implemented correct solution
    With tests!

--- functional_programming_scala/week4/pattern_matching/src/main/scala/Pattern_Matching.scala
+++ functional_programming_scala/week4/pattern_matching/src/main/scala/Pattern_Matching.scala
@@ -10,18 +10,16 @@ case class Prod(e1: Expr, e2: Expr) extends Expr
 object Util{
 def show(e: Expr) : String = e match {
 	case Number(n) => n.toString()
-	//case Sum(Prod(e1),e2) => "("+show(e1)+")" + " + " + show(e2)
-	//case Sum(e1,Prod(e2)) => show(e1) + " + " + "("+show(e2)+")"
 	//case Sum(e1,e2) => show(e1) + " + " + show(e2)
-	case Sum(e1,e2) 
+	case Prod(e1,e2) 
 	=> {
 		(e1,e2) match {
-		    case (Prod(ee1,ee2),Prod(ee3,ee4)) => "("+show(e1)+")" + " + " + "("+show(e2)+")"
-			case (Prod(ee1,ee2),_) =>"("+show(e1)+")" + " + " + show(e2)
-			case (_,Prod(ee1,ee2)) => show(e1) + " + " + "("+show(e2)+")"
-			case _ => show(e1) + " + " + show(e2)
+		    case (Sum(ee1,ee2),Sum(ee3,ee4)) => "("+show(e1)+")" + " * " + "("+show(e2)+")"
+			case (Sum(ee1,ee2),_) =>"("+show(e1)+")" + " * " + show(e2)
+			case (_,Sum(ee1,ee2)) => show(e1) + " * " + "("+show(e2)+")"
+			case _ => show(e1) + " * " + show(e2)
 		}
 	}
 	case Var(name) => name
-	case Prod(e1,e2) => show(e1) + " * "+show(e2)
+	case Sum(e1,e2) => show(e1) + " + "+show(e2)
 }}
--- functional_programming_scala/week4/pattern_matching/src/test/scala/Pattern_MatchingSpec.scala
+++ functional_programming_scala/week4/pattern_matching/src/test/scala/Pattern_MatchingSpec.scala
@@ -20,21 +20,21 @@ class Pattern_MatchingSpec extends FlatSpec {
 	}
 	"show(Sum(Prod(Number(4),Number(5)),Sum(Number(4),Number(5)))) " should " return \"(4 * 5) + 4 + 5\" " in {
 		val result=Util.show(Sum(Prod(Number(4),Number(5)),Sum(Number(4),Number(5))))
-		assert(result === "(4 * 5) + 4 + 5")
+		assert(result === "4 * 5 + 4 + 5")
 	}
 	
-	"show(Sum(Prod(Number(2),Var(\"x\")),Var(\"y\"))) " should " return \"(2 * x) + y\" " in {
+	"show(Sum(Prod(Number(2),Var(\"x\")),Var(\"y\"))) " should " return \"2 * x + y\" " in {
 		val result=Util.show(Sum(Prod(Number(2),Var("x")),Var("y")))
-		assert(result === "(2 * x) + y")
+		assert(result === "2 * x + y")
 	}
 	
-	"show(Sum(Var(\"y\")),Prod(Number(2),Var(\"x\"))) " should " return \"y + (2 * x)\" " in {
-		val result=Util.show(Sum(Var("y"),Prod(Number(2),Var("x"))))
-		assert(result === "y + (2 * x)")
+	"show(Prod(Sum(Number(2),Var(\"x\")),Var(\"y\"))) " should " return \"y + 2 * x\" " in {
+		val result=Util.show(Prod(Sum(Number(2),Var("x")),Var("y")))
+		assert(result === "(2 + x) * y")
 	}
-	
-	"show(Sum(Prod(Number(2),Var(\"y\")),Prod(Number(2),Var(\"x\")))) " should " return \"(2 * y) + (2 * x)\" " in {
-		val result=Util.show(Sum(Prod(Number(2),Var("y")),Prod(Number(2),Var("x"))))
-		assert(result === "(2 * y) + (2 * x)")
+	//ko?
+	"show(Prod(Sum(Number(2),Var(\"x\")),Sum(Var(\"y\"),Number(2)))) " should " return \"(2 + x) * (y + 2)\" " in {
+		val result=Util.show(Prod(Sum(Number(2),Var("x")),Sum(Var("y"),Number(2))))
+		assert(result === "(2 + x) * (y + 2)")
 	}
 }
