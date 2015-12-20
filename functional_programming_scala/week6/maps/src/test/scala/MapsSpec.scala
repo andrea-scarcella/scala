06931f556fb93bcb8a839b8366fbc69eb83ec5d5
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2015-12-20 23:02:22 +0100

    Added more tests, improved test description

--- functional_programming_scala/week6/maps/src/test/scala/MapsSpec.scala
+++ functional_programming_scala/week6/maps/src/test/scala/MapsSpec.scala
@@ -3,10 +3,17 @@ import org.scalatest._
 import Matchers._
 class MapsSpecs extends FlatSpec {
 
-    "Poly.+ " should " sum coefficients of terms with same power" in{
+    "Poly.+ " should " sum coefficients of terms with same exponent" in{
 		val p1=new Poly(0->3.0);
 		val p2=new Poly(0->6.0);
 		val expected=new Poly(0->9.0);
+		val actual = p1 + p2;
+		assert(expected.terms === actual.terms);
+	}
+	it should " not sum coefficients of terms with different exponent" in{
+		val p1=new Poly(0->3.0);
+		val p2=new Poly(1->6.0);
+		val expected=new Poly(0->3.0,1 -> 6.0);
 		val actual = p1 + p2;
 		assert(expected.terms === actual.terms);
 	}
